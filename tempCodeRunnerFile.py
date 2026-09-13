a569dd9ec40e7df851eaf2e8a5489bd9e408c7d2
Harishyam Tripathi <harishyamtripathi10@gmail.com>
2026-09-14 00:59:37 +0530

    new

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,7 @@
-while 5>4:
-    print("it is infinite loop")
+num = int (input("Enter your number:"))
+sum = 0
+while num > 0:
+    ld = num % 10
+    sum = sum + ld
+    num = num // 10
+print(sum)
