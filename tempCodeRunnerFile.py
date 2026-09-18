0aa744bec400597b05116f68cd7a1927ea025e6c
Harishyam Tripathi <harishyamtripathi10@gmail.com>
2026-09-18 23:58:51 +0530

    new

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -3,7 +3,7 @@ s = input("Enter a string: ")
 count = 0
 
 for i in s:
-    if i in "AEIOUaeiou":
+    if i.isalpha() and i not in "AEIOUaeiou":
         count = count + 1
 
-print("Number of vowels =", count)
+print("Number of consonants =", count)
