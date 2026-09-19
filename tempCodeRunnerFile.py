6afb7b9f7e7133ab3363d1f1150068fa07bf387a
Harishyam Tripathi <harishyamtripathi10@gmail.com>
2026-09-19 23:55:25 +0530

    new

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,8 +1,6 @@
 numbers = [10, 25, 5, 40, 15]
-
-largest = numbers[0]
+smallest = numbers[0]
 for i in numbers:
-    if i > largest:
-        largest = i
-
-print("Largest =", largest)
+    if i < smallest:
+        smallest = i
+print("Smallest =", smallest)
