9fb0f301ed1d6a16856c08891a00d258204c4a77
Harishyam Tripathi <harishyamtripathi10@gmail.com>
2026-09-21 23:49:40 +0530

    new

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,4 +1,7 @@
-num = int(input("Enter a number: "))
-
-for i in range(1, 11):
-    print(num, "x", i, "=", num * i)
+num = int(input("Enter your number:"))
+rev = 0
+while num > 0:
+    ld = num % 10
+    rev = 10*rev + ld
+    num = num// 10
+print("Reverse of a number is",rev)
