1cd05457533ce3868af51a7086d86ec3daf051cc
Harishyam Tripathi <harishyamtripathi10@gmail.com>
2026-09-23 23:57:31 +0530

    new

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,12 +1,4 @@
-def fibonacci():
-    length = int(input("Enter your length of fibonacci:"))
-    n1,n2 = 0,1
-    print(n1,n2,end = " ")
-    for i in range (length -2):
-        next = n1 + n2
-        print(next, end=" ")
-        n1 = n2
-        n2 = next
-fibonacci()
-print()
-fibonacci()
+a = 5
+b = 10
+res =a*b
+print("Result of the multiply is",res)
